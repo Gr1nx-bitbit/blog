9032990002a0c5932f20f0df454a987f1a91a421
Gr1nx <gr1nx614@gmail.com>
2024-07-12 14:52:21 -0700

    made some progress with the comments. need to add some data and test it out though

--- database/comments.go
+++ database/comments.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 // this function will be called whenever a new blog is made so
@@ -11,8 +12,28 @@ import (
 func createCommentTable(db *sql.DB, blogName string) {
 	// sqlite doesn't have a boolean keyword. It instead sotres 0's and 1's as falses and trues, respectively. It can however recognize the
 	// TRUE and FALSE keyword so it'll automatically make those 0's and 1's.
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS " + blogName + "CommentTable" + " (\nCommentID INTEGER PRIMARY KEY,\n OwnerReference INTEGER,\n Leaf INTEGER,\n Comment TEXT\n)")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS " + blogName + "CommentTable" + " (\nCommentID INTEGER PRIMARY KEY,\n OwnerReference INTEGER,\n Leaf INTEGER,\n HasChildren INTEGER,\n Comment TEXT\n)")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func addComment(tableName string, ownerRef int, comment string, db *sql.DB) {
+	// we have to check whether the comment is a leaf
+	rows, err := db.Query("SELECT HasChildren FROM " + tableName + " WHERE CommentID='" + string(ownerRef) + "'")
+	if err != nil {
+		panic(err)
+	}
+
+	var children int
+	for rows.Next() {
+		rows.Scan(&children)
+	}
+
+	if children == 1 {
+		fmt.Println("Children Present")
+	} else {
+		fmt.Println("This comment is a first child and leaf.")
+	}
+
 }
--- database/db.go
+++ database/db.go
@@ -6,6 +6,11 @@ import (
 	"os"
 )
 
+// this function can be used to initialize a blog table in the database which
+// there will ever only be ONE of. We can add some error handiling where it
+// will check a specific path provided for a db instead of relying on the user.
+// dbExists is self-explanatory as well as dbPath. pathReturn will return you
+// the path the db has been set to so the rest of the program can use it
 func BlogInit(dbExists bool, dbPath string) *sql.DB {
 	return initializeBlogTable(dbExists, dbPath)
 }
@@ -20,6 +25,9 @@ func Connect(driver string, dbPath string) *sql.DB {
 	return db
 }
 
+// this function will just add a new blog entry to the db. "blogName" is the name / title
+// of the blog while "blogFilePath" is the path to the blog's content in the file system.
+// db is an instance of the database since it will have already been created
 func AddBlog(blogName string, file string, db *sql.DB) {
 	addBlog(blogName, file, db)
 }
