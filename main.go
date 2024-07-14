67a06bf793d0dce35701dba31881e5241ff537d6
Gr1nx <gr1nx614@gmail.com>
2024-07-13 20:46:48 -0700

    fixed the problem with blog for now. we probably want the db uri to be an argument passed in instead of hardcoded

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"github.com/Gr1nx-bitbit/blog/database"
 	"github.com/Gr1nx-bitbit/blog/server"
-	// "github.com/Gr1nx-bitbit/blog/server"
 )
 
 func cool() {
-	db := database.BlogInit(database.IsDB("./blog"), "")
+	db := database.BlogInit(database.IsDB("./blog.db"), "")
 	database.AddBlog("test", "./test.txt", db)
 	database.AddComment("testCommentTable", -1, "Does this work?", db)
 	database.PrintBlogs(db)
